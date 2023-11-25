b444753171855c51339cc3aae2e9523e7fd49c3f
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2023-11-25 12:26:48 +0200

    removed warnings

--- NetMarket/ClientApp/src/components/CategoryList.jsx
+++ NetMarket/ClientApp/src/components/CategoryList.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
 //import axios from 'axios';
 
 const CategoryList = () => {
-    const [categories, setCategories] = useState([]);
+    const categories = [];
 
     //useEffect(() => {
     //    const fetchCategories = async () => {
