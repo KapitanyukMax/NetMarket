990c0dff699be81a1e5ad35919ea8c4177f82cb2
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2023-11-25 11:39:45 +0200

    Remove axios

--- NetMarket/ClientApp/src/components/CategoryList.jsx
+++ NetMarket/ClientApp/src/components/CategoryList.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 const CategoryList = () => {
     const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-                const response = await axios.get('https://localhost:44447/category');
-                setCategories(response.data);
-            } catch (error) {
-                console.error('Error fetching categories:', error);
-            }
-        };
+    //useEffect(() => {
+    //    const fetchCategories = async () => {
+    //        try {
+    //            const response = await axios.get('https://localhost:44447/category');
+    //            setCategories(response.data);
+    //        } catch (error) {
+    //            console.error('Error fetching categories:', error);
+    //        }
+    //    };
 
-        fetchCategories();
-    }, []);
+    //    fetchCategories();
+    //}, []);
 
     return (
         <div>
